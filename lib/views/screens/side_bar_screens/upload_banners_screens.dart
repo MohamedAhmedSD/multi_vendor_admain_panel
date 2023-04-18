2264391cc10556eab9a1255fe61181b7324598f0
MohamedAhmedSD <btoox8python@gmail.com>
2023-04-18 23:14:01 +0200

    [banners screen]

--- lib/views/screens/side_bar_screens/upload_banners_screens.dart
+++ lib/views/screens/side_bar_screens/upload_banners_screens.dart
@@ -8,13 +8,38 @@ class UploadScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Container(
-        alignment: Alignment.topLeft,
-        padding: EdgeInsets.all(10),
-        child: Text(
-          "upload banners",
-          style: TextStyle(fontWeight: FontWeight.w700, fontSize: 36),
-        ),
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            padding: EdgeInsets.all(10),
+            child: Text(
+              "Banners",
+              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 36),
+            ),
+          ),
+          //! banner && 2 buttons
+          Divider(
+            color: Colors.grey,
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(14.0),
+                child: Container(
+                  height: 140,
+                  width: 140,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade500,
+                    border: Border.all(color: Colors.grey.shade800),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(child: Text("Babbers"),),
+                ),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
