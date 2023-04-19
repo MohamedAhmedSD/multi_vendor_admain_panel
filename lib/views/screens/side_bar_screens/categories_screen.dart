3913d16b1da32d254e8be695a0e8426f2efe4682
MohamedAhmedSD <btoox8python@gmail.com>
2023-04-19 11:43:30 +0200

    category screen

--- lib/views/screens/side_bar_screens/categories_screen.dart
+++ lib/views/screens/side_bar_screens/categories_screen.dart
@@ -12,13 +12,79 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Container(
-        alignment: Alignment.topLeft,
-        padding: EdgeInsets.all(10),
-        child: Text(
-          "Cat",
-          style: TextStyle(fontWeight: FontWeight.w700, fontSize: 36),
-        ),
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            padding: EdgeInsets.all(10),
+            child: Text(
+              "Categories",
+              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 36),
+            ),
+          ),
+          //! banner && 2 buttons
+          Divider(
+            color: Colors.grey,
+          ),
+          Row(
+            children: [
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(14.0),
+                    child: Container(
+                      height: 140,
+                      width: 140,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade500,
+                        border: Border.all(color: Colors.grey.shade800),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      //! display picked image
+                      //! we use image.memory() => take bytes
+                      //? restart app if not work
+                      child: Center(
+                        child: Text("Category"),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.yellow.shade900),
+                    onPressed: () {},
+                    child: Text("Upload Image"),
+                  ),
+                ],
+              ),
+              SizedBox(
+                width: 30,
+              ),
+              //? add TFF wrap under => flexible => sizedbox
+              Flexible(
+                child: SizedBox(
+                  width: 180,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        labelText: "Enter Category Name",
+                        hintText: "Enter Category Name,"),
+                  ),
+                ),
+              ),
+              //! buttons
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.yellow.shade900),
+                onPressed: () {
+                  //! upload to cloud which automaticlly upload first to storage
+                },
+                child: Text("Save"),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
